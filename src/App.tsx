e9f4c77f2cdd64349951a657979a4778d376f727
Sapfir0 <sapfr999999@yandex.ru>
2021-09-19 00:48:13 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Drawable } from './Drawable';
+
+function App() {
+    return (
+        <div className="App">
+            <Drawable />
+        </div>
+    );
+}
+
+export default App;
